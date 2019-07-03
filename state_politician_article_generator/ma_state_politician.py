9bd55bef6361737265933033308d71d5fa35ae84
Will Schlitzer <schlitzer90@gmail.com>
2019-07-03 17:12:36 +0900

    Update for multiple states

--- state_politician_article_generator/ma_state_politician.py
+++ state_politician_article_generator/ma_state_politician.py
@@ -1,31 +1,28 @@
-politcian_last = "Olsen"
-politician_first = "Jim"
+politcian_last = "Gouveia"
+politician_first = "Tami"
 state_senator = False
-state = "Oklahoma"
-state_abbrev = "OK"
-named_districts = False
-district_no = "2"
-district_name = ""
-town_list = ["Sequoyah County"]
-democrat = False
+state = "Massachusetts"
+state_abbrev = "MA"
+district_no = "14th"
+district_name = "Middlesex"
+town_list = ["Acton", "Carlisle", "Chelmsford", "Concord"]
+democrat = True
 term_start = "2019"
 successor = ""
-predecessor = "[[John R. Bennett]]"
-alma_mater = "State University of New York, Albany"
+predecessor = ""
+alma_mater = "[[Mount Holyoke College]]"
 spouse = ""
-residence = ""
-date_accesssed = "June 29, 2019"
-male = True
-official_portrait = True
-committee_in_name=False
+residence = "Acton"
+date_accesssed = "May 3, 2019"
+male = False
 committee_list = [
-    "Administrative Rules",
-    "A&B Public Safety",
-    "Judiciary",
-    "Public Safety",
+    "Joint Committee on Children, Families and Persons with Disabilities",
+    "Joint Committee on Consumer Protection and Professional Licensure",
+    "Joint Committee on Export Development"
+    "Joint Committee on Mental Health, Substance Use and Recovery",
+
+
 ]
-
-
 
 
 def get_pronouns(male):
@@ -35,7 +32,7 @@ def get_pronouns(male):
         return "she", "her"
 
 
-def gen_committee_string(committee_list, committee_in_name):
+def gen_committee_string(committee_list):
     committee_string = ""
     for i, committee in enumerate(committee_list):
         if i == len(committee_list) - 1 and len(committee_list) > 1:
@@ -43,8 +40,6 @@ def gen_committee_string(committee_list, committee_in_name):
         else:
             committee_string += "the "
         committee_string += committee
-        if not committee_in_name:
-            committee_string += " Committee"
         if i < len(committee_list) - 1 and len(committee_list) > 2:
             committee_string += ", "
     return committee_string
@@ -89,10 +84,8 @@ def gen_office(state, state_senator):
             "[[State Representative]]",
         )
 
-if residence:
-    residence_link = "[[" + residence + ", " + state + "|" + residence + "]]"
-else:
-    residence_link = ""
+
+residence_link = "[[" + residence + ", " + state + "|" + residence + "]]"
 if alma_mater:
     alma_mater_link = "[[" + alma_mater + "]]"
 else:
@@ -103,42 +96,13 @@ pronoun, pos_pronoun = get_pronouns(male)
 town_string, town_plural = gen_town_string(town_list, state)
 party, party_link = gen_party(democrat)
 
-
-if named_districts:
-    district = district_no + " " + district_name + " District"
-else:
-    district = "District " + district_no
+district = district_no + " " + district_name + " District"
 district_link = "[[" + district + "]]"
 office, office_link, title, title_link = gen_office(state, state_senator)
 short_desc = "{{short description|" + state + " politician}}\r"
 template = "\r{{" + office + "|state=collapsed}}\r"
-committee_string = gen_committee_string(committee_list, committee_in_name)
-
-if official_portrait:
-    file_name = politician_first + "_" + politcian_last + "_official_portrait.jpg"
-    file_caption = (
-        politician_first
-        + " "
-        + politcian_last
-        + "'s official portrait from the "
-        + office
-        + "."
-    )
-    pic_info = "filename\r\r"
-    pic_info += file_name
-    pic_info += "\r\r"
-    pic_info += "caption\r\r"
-    pic_info += file_caption
-    #following is for Oklahoma congressmen
-    pic_info += "author\r\rOklahoma Legislative Service Bureau"
-    pic_info += "template\r\r{{PD-OK-LSBPD}}"
-    with open("pic_info.txt", "w") as file:
-        file.write(pic_info)
-
+committee_string = gen_committee_string(committee_list)
 infobox_name = "|name = " + politician_first + " " + politcian_last + "\r"
-infobox_image = "|image ="
-if official_portrait:
-    infobox_image += file_name
 infobox_office = (
     "|office = " + "Member of the " + office_link + " from the " + district + "\r"
 )
@@ -153,7 +117,6 @@ infobox_predecessor = "|predecessor = " + predecessor + "\r"
 pol_infobox = (
     "{{Politician infobox"
     + infobox_name
-    + infobox_image
     + infobox_office
     + infobox_party
     + infobox_term_start
@@ -183,11 +146,7 @@ main_text += politcian_last + " serves on " + committee_string + "."
 
 def gen_cat_string(state, party, office, residence, alma_mater):
     cat_string = ""
-    cat_list = [
-        "Living people",
-        "21st-century politicians",
-        "21st-century American politicians",
-    ]
+    cat_list = ["Living people","21st-century politicians", "21st-century American politicians"]
     cat_list.append(state + " politicians")
     cat_list.append(state + " " + party + "s")
     cat_list.append("Members of the " + office)
@@ -250,14 +209,14 @@ def create_web_citation(
     return citation
 
 
-ref1_name = "swtimes"
+ref1_name = "about_tami"
 ref1_last = ""
 ref1_first = ""
-ref1_title = "Sallisaw businessman files for Oklahoma House seat"
+ref1_title = "About Tami"
 ref1_work = ""
-ref1_publisher = "Times Record"
-ref1_url = "https://www.swtimes.com/news/20180425/sallisaw-businessman-files-for-oklahoma-house-seat"
-ref1_date = "April 25, 2018"
+ref1_publisher = "State Representative Tami Gouveia"
+ref1_url = "https://www.tamigouveia.com/about-tami"
+ref1_date = "2018"
 ref1_citation = create_web_citation(
     name=ref1_name,
     author_last=ref1_last,
@@ -272,14 +231,14 @@ ref1_citation = create_web_citation(
 # ref1_citation = ''
 
 
-ref2_name = "muskogee_politico"
-ref2_last = "Faught"
-ref2_first = "Jamison"
-ref2_title = "Jim Olsen running for State House District 2"
+ref2_name = "gouveia_committees"
+ref2_last = ""
+ref2_first = ""
+ref2_title = "Tami L. Gouveia: Committees"
 ref2_work = ""
-ref2_publisher = "Muskogee Politico"
-ref2_url = "https://www.muskogeepolitico.com/2018/04/jim-olsen-running-for-state-house.html"
-ref2_date = "April 25, 2018"
+ref2_publisher = "The Commonwealth of Massachusetts"
+ref2_url = "https://malegislature.gov/Legislators/Profile/TLG1/Committees"
+ref2_date = "2019"
 ref2_citation = create_web_citation(
     name=ref2_name,
     author_last=ref2_last,
@@ -294,13 +253,13 @@ ref2_citation = create_web_citation(
 # ref2_citation = ''
 
 
-ref3_name = "rep_page"
+ref3_name = "gouveia_district"
 ref3_last = ""
 ref3_first = ""
-ref3_title = "Representative Jim Olsen District 2 - Republican"
+ref3_title = "Tami L. Gouveia: District"
 ref3_work = ""
-ref3_publisher = "Oklahoma State Legislature"
-ref3_url = "https://www.okhouse.gov/Members/District.aspx?District=2"
+ref3_publisher = "The Commonwealth of Massachusetts"
+ref3_url = "https://malegislature.gov/Legislators/Profile/TLG1/District"
 ref3_date = "2019"
 ref3_citation = create_web_citation(
     name=ref3_name,
@@ -315,14 +274,14 @@ ref3_citation = create_web_citation(
 )
 # ref3_citation = ""
 
-ref4_name = "laboeuf_bio"
-ref4_last = ""
-ref4_first = ""
-ref4_title = "David Henry Argosky Laboeuf: Biography"
+ref4_name = "wicked_local"
+ref4_last = "Razzaq"
+ref4_first = "Zane"
+ref4_title = "In 6th Middlesex, Maria Gouveia becomes first Korean-American in Mass. House"
 ref4_work = ""
-ref4_publisher = "The Commonwealth of Massachusetts"
-ref4_url = "https://malegislature.gov/Legislators/Profile/DAL1/Biography"
-ref4_date = "2019"
+ref4_publisher = "[[Wicked Local]]"
+ref4_url = "https://www.wickedlocal.com/news/20181106/in-6th-middlesex-maria-Gouveia-becomes-first-korean-american-in-mass-house"
+ref4_date = "November 7, 2018"
 ref4_citation = create_web_citation(
     name=ref4_name,
     author_last=ref4_last,
@@ -336,27 +295,6 @@ ref4_citation = create_web_citation(
 )
 ref4_citation = ""
 
-ref5_name = "meet_david"
-ref5_last = ""
-ref5_first = ""
-ref5_title = "Meet David"
-ref5_work = ""
-ref5_publisher = "Laboeuf Democrat for State Rep"
-ref5_url = "https://www.davidleboeuf.org/about"
-ref5_date = "2019"
-ref5_citation = create_web_citation(
-    name=ref5_name,
-    author_last=ref5_last,
-    author_first=ref5_first,
-    work=ref5_work,
-    publisher=ref5_publisher,
-    title=ref5_title,
-    url=ref5_url,
-    date=ref5_date,
-    date_accessed=date_accesssed,
-)
-ref5_citation = ""
-
 page_text = short_desc + pol_infobox + main_text
 if ref1_citation:
     page_text += ref1_citation
@@ -366,31 +304,27 @@ if ref3_citation:
     page_text += ref3_citation
 if ref4_citation:
     page_text += ref4_citation
-if ref5_citation:
-    page_text += ref5_citation
+
 page_text += "\r==References==\r{{reflist}}"
 page_text += template
 page_text += "\r{{DEFAULTSORT: " + politcian_last + ", " + politician_first + "}}\r"
 page_text += cat_string
 # For Massachusetts use only
-#page_text += "\r{{Massachusetts-MARepresentative-stub}}"
+page_text += "\r{{Massachusetts-MARepresentative-stub}}"
 with open("page.txt", "w") as file:
     file.write(page_text)
 
 talk_text = (
-    "{{talkheader|search=no}}\r{{WikiProject Biography|living=yes|class=stub|auto=yes|listas="
+    "{{talkheader|search=yes}}\r{{WikiProject Biography|living=yes|class=stub|auto=yes|listas="
     + politcian_last
     + ", "
     + politician_first
-    + "}}\r{{WikiProject United States|class=Stub|importance=Low}}\r"
-# Used only if state has a WikiProject under WP United States
-#    + "}}\r{{WikiProject United States|class=Stub|importance=Low|"
-#    + state_abbrev
-#    + "=Yes|"
-#    + state_abbrev
-#    + "-importance=Low}}\r"
+    + "}}\r{{WikiProject United States|class=Stub|importance=Low|"
+    + state_abbrev
+    + "=Yes|"
+    + state_abbrev
+    + "-importance=Low}}\r"
     + "{{User:MiszaBot/config | algo = old(30d) | archive = {{SUBST:FULLPAGENAME}}/Archive %(counter)d | counter = 1 | maxarchivesize = 150K | archiveheader = {{Automatic archive navigator}} | minthreadstoarchive = 1 | minthreadsleft = 4 }}"
 )
 with open("talk.txt", "w") as file:
-    file.write(talk_text)
-
+file.write(talk_text)
