26078c10852770efceee1db3c47e21a22aa874e6
Will Schlitzer <schlitzer90@gmail.com>
2020-05-31 18:56:15 +0900

    Add error checking

--- template_text_replacer/plain_to_unbulleted_template.py
+++ template_text_replacer/plain_to_unbulleted_template.py
@@ -1,10 +1,12 @@
 import re
+import pyperclip
+import time
 
 pretext = "{{Unbulleted list|"
 posttext = "}}"
 
-text = """[[Eileen Collins|Eileen M. Collins]]<br/>[[Jeffrey Ashby|Jeffrey S. Ashby]]<br/>[[Michel Tognini]]<br/>[[Steven Hawley|Steven A. Hawley]]<br/>[[Catherine Coleman|Catherine G. Coleman]]"""
-
+#text = """[[John O. Creighton]]<br/>[[John H. Casper]]<br/>[[Pierre J. Thuot]]<br/>[[David C. Hilmers]]<br/>[[Richard M. Mullane]]"""
+text = input("Astronauts: ")
 #print(text[-3:])
 new_text = text.replace("<br />", "<br/>").replace("<br>", "<br/>")
 wiki_names = new_text.split("<br/>")
@@ -12,10 +14,16 @@ name_string = "|".join(wiki_names)
 final_string = pretext + name_string + posttext
 if (text[:3] == "[[[") or (text[0:2] != "[[") or (text[-3:] == "]]]") or (text[-2:] != "]]"):
     print("Check punctuation at the beginning/end")
+    pyperclip.copy("bad copy/paste")
 else:
     print(final_string)
     print()
-    print("modifying crew list to use unbulleted list template; standardizing spaceflight articles for HTML list elements")
+    about_msg = "modifying crew list to use unbulleted list template; standardizing spaceflight articles for HTML list elements"
+    print(about_msg)
+    pyperclip.copy(about_msg)
+    time.sleep(3)
+    pyperclip.copy(final_string)
+
 
 ##
 #or (text[-3:] == "]]]") or (text[-2:] != "]]")
