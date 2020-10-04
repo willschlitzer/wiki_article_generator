cbb48b79e861eaa5e7a7f32313aa77cd182f1a96
Will Schlitzer <schlitzer90@gmail.com>
2020-10-04 09:06:00 +0100

    Refactor for class to create web citation

---
+++ us_military_general_article_generator/web_citation_creator.py
@@ -0,0 +1,16 @@
+class WebCitation():
+    def __init__(self, ref_name, title, url, date_accessed, author_last = "", author_first = "", work = "", publisher = "", date = ""):
+        self.ref_name = ref_name
+        self.title = " |title = " + title
+        self.author_last = " |last = " + author_last
+        self.author_first = " |first = " + author_first
+        self.work = " |work = " + work
+        self.publisher = " |publisher = " + publisher
+        self.date = " |date = " + date
+        self.url = " |url = " + url
+        self.date_accessed = " |date_accessed = " + date_accessed
+        self.create_citation()
+
+    def create_citation(self):
+        cite_info = str(self.title + self.author_last + self.author_first + self.work + self.publisher + self.date + self.url + self.date_accessed)
+        self.citation = "<ref name=" + self.ref_name + ">{{cite web" + cite_info.replace("  ", " ") + "}}</ref>"
